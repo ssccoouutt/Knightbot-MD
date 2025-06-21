01e0e7313ac8dfb2d00bd1732e3c667481d90b0f
ssccoouutt <144871295+ssccoouutt@users.noreply.github.com>
2025-06-21 16:42:30 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,3 @@
-const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.0.6",
-};
-
-module.exports = settings;
+module.exports = {
+  botName: "Simple Bot"
+}
